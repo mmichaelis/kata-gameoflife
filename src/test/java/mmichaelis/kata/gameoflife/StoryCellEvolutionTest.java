2132b4ec4a08cd332b69e69c2cc524039c7c6ce1
Mark Michaelis <mark.michaelis@coremedia.com>
2013-01-10 21:31:57 +0100

    Another death scenario test.

--- src/test/java/mmichaelis/kata/gameoflife/StoryCellEvolutionTest.java
+++ src/test/java/mmichaelis/kata/gameoflife/StoryCellEvolutionTest.java
@@ -11,27 +11,43 @@ import static org.mockito.Mockito.mock;
  * @since 1.0
  */
 public class StoryCellEvolutionTest {
-  private static final int MINIMUM_LINK_COUNT = 2;
 
   /* =======[ S C E N A R I O S ]======= */
 
   @Test
-  public void scenario_cell_dies_on_evolution_with_too_few_neighboring_cells() throws Exception {
+  public void scenario_dead_cell_stays_dead_on_evolution_with_too_few_neighboring_cells() throws Exception {
     final References.Reference<Cell> cellReference = ref();
     final References.Reference<Integer> linkCountReference = ref();
-    given_a_cell_C(cellReference);
+    given_a_dead_cell_C(cellReference);
     given_minimum_link_count_N(linkCountReference);
     then_cell_C_dies_with_less_than_N_links(cellReference, linkCountReference);
   }
 
+  @Test
+  public void scenario_dead_cell_stays_dead_on_evolution_with_too_many_neighboring_cells() throws Exception {
+    final References.Reference<Cell> cellReference = ref();
+    final References.Reference<Integer> linkCountReference = ref();
+    given_a_dead_cell_C(cellReference);
+    given_maximum_link_count_N(linkCountReference);
+    then_cell_C_dies_with_more_than_N_links(cellReference, linkCountReference);
+  }
+
   /* =======[ S T E P S ]======= */
 
-  private void given_a_cell_C(final References.Reference<Cell> cellReference) {
+  private static final int MINIMUM_LINK_COUNT = 2;
+  private static final int MAXIMUM_LINK_COUNT = 3;
+  private static final int MAXIMUM_LINK_TEST_COUNT = MAXIMUM_LINK_COUNT + 1;
+
+  private void given_a_dead_cell_C(final References.Reference<Cell> cellReference) {
     cellReference.set(new CellImpl());
   }
 
   private void given_minimum_link_count_N(final References.Reference<Integer> linkCountReference) {
     linkCountReference.set(MINIMUM_LINK_COUNT);
+  }
+
+  private void given_maximum_link_count_N(final References.Reference<Integer> linkCountReference) {
+    linkCountReference.set(MAXIMUM_LINK_COUNT);
   }
 
   private void when_cell_C_performs_an_evolution_step(final References.Reference<Cell> cellReference) {
@@ -49,6 +65,14 @@ public class StoryCellEvolutionTest {
 
   private void then_cell_C_dies_with_less_than_N_links(final References.Reference<Cell> cellReference, final References.Reference<Integer> linkCountReference) {
     for (int i = 0; i < linkCountReference.get() - 1; i++) {
+      when_cell_C_is_linked_to_N_cells(cellReference, i);
+      when_cell_C_performs_an_evolution_step(cellReference);
+      then_cell_C_dies(cellReference);
+    }
+  }
+
+  private void then_cell_C_dies_with_more_than_N_links(final References.Reference<Cell> cellReference, final References.Reference<Integer> linkCountReference) {
+    for (int i = linkCountReference.get() + 1; i < MAXIMUM_LINK_TEST_COUNT; i++) {
       when_cell_C_is_linked_to_N_cells(cellReference, i);
       when_cell_C_performs_an_evolution_step(cellReference);
       then_cell_C_dies(cellReference);
